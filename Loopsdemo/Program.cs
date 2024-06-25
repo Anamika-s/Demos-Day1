c65591b4538027309efb3811e154c04b3412f07d
Anamika Sawhney <anamika.sawhney22@gmail.com>
2024-06-25 14:54:02 +0530

    added loops

---
+++ Loopsdemo/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Loopsdemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Loops
+            // whenyou want to repeat some statements
+            // for do-while while foreach
+            // every loop has 3 statements
+            // 1. Initialization
+            // 2 Condition
+            // 3 inc / dec statement
+
+            // print numbers from 1 to 10
+            // do while loop
+
+            Console.WriteLine("using do - while loop");
+            int n = 1;
+            do
+            {
+                Console.WriteLine(n);
+                n++;
+            } while (n <= 10);
+
+            Console.WriteLine("using while loop");
+            n = 1;
+            while(n<=10)
+            {
+                Console.WriteLine(n);
+                n++;
+            }
+            Console.WriteLine("using for loop");
+            for (int i = 1; i <= 10; i++)
+                Console.WriteLine(i);
+
+        }
+    }
+}
---
+++ Loopsdemo/SumOfNumbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loopsdemo
+{
+    internal class SumOfNumbers
+    {
+        static void Main()
+        {
+            int sum = 0;
+            int num;
+            //for(int i = 0;i<=10;i++)
+            //{
+            //    num = sbyte.Parse(Console.ReadLine());
+            //    sum += num;
+            //}
+            // Take sum of positive numbers and quit once u get 0
+            for (int i = 0; i <= 10; i++)
+            {
+                num = sbyte.Parse(Console.ReadLine());
+                if (num < 0) continue;
+                if (num == 0) break;
+                sum += num;
+            }
+
+            float average = (float)sum / 10;
+            Console.WriteLine($"Sum is {sum} Average is {average}");
+        }
+    }
+}
